a2f752e127951b7fe391c7237662f6cc390c3634
caiquegressoni <caiquegressoni@gmail.com>
2026-01-23 16:54:49 -0300

    Mentoria e Curso

--- DesafiosDeCodigo/Resolucoes/src/Main.java
+++ DesafiosDeCodigo/Resolucoes/src/Main.java
@@ -1,21 +1,82 @@
-import java.util.Scanner;
+import java.util.*;
+
+abstract class Consultor implements Comparable<Consultor> {
+    public String nome;
+    public String nivel;
+    public String especialidade;
+
+    public Consultor(String nome, String nivel, String especialidade) {
+        this.nome = nome;
+        this.nivel = nivel;
+        this.especialidade = especialidade;
+    }
+
+    @Override
+    public int compareTo(Consultor outro) {
+        return this.nome.compareTo(outro.nome);
+    }
+}
+
+class ConsultorBackend extends Consultor {
+    public ConsultorBackend(String nome, String nivel) {
+        super(nome, nivel, "Backend");
+    }
+}
+
+class ConsultorFrontend extends Consultor {
+    public ConsultorFrontend(String nome, String nivel) {
+        super(nome, nivel, "Frontend");
+    }
+}
+
+class ConsultorDados extends Consultor {
+    public ConsultorDados(String nome, String nivel) {
+        super(nome, nivel, "Dados");
+    }
+}
+
+class Equipe {
+    public String nome;
+    public List<Consultor> consultores = new ArrayList<>();
+
+    public Equipe(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionar(Consultor c) {
+        consultores.add(c);
+    }
+
+    public List<Consultor> ordenados() {
+        List<Consultor> lista = new ArrayList<>(consultores);
+        Collections.sort(lista);
+        return lista;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite: ");
-        String codigo = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-        if(codigo.equals("EC2")){
-          System.out.println("EC2");
-        }else if(codigo.equals("S3")){
-          System.out.println("S3");
-        }else if(codigo.equals("Lambda")){
-          System.out.println("Lambda");
-        }else{
-          System.out.println("codigo invalido");
+        Map<String, Equipe> equipes = new HashMap<>();
+
+        Equipe alpha = new Equipe("Alpha");
+        alpha.adicionar(new ConsultorDados("Lucas", "Pleno"));
+        alpha.adicionar(new ConsultorBackend("Bruno", "Senior"));
+        alpha.adicionar(new ConsultorFrontend("Maria", "Junior"));
+        equipes.put("Alpha", alpha);
+
+        Equipe beta = new Equipe("Beta");
+        beta.adicionar(new ConsultorBackend("Joao", "Junior"));
+        beta.adicionar(new ConsultorDados("Ana", "Senior"));
+        equipes.put("Beta", beta);
+
+        String nomeEquipe = sc.nextLine();
+        Equipe equipe = equipes.get(nomeEquipe);
+
+        // TODO: Se a equipe existir, imprima os consultores ordenados; caso contrário "Equipe nao encontrada"
+        if(equipe.equals("Alpha")){
+          System.out.println(equipes);
         }
-        // TODO: Verifique se o código corresponde exatamente a "EC2", "S3" ou "Lambda" e imprima o serviço correspondente.
-        // Caso contrário, imprima "Codigo invalido".
     }
 }
