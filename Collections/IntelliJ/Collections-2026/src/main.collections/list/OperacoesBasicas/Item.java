eb4faa7c43d51c123912a5a357377d9f856bd2c3
caiquegressoni <caiquegressoni@gmail.com>
2026-04-10 12:20:06 -0300

    Teste pos alteracao de maquina

--- Collections/IntelliJ/Collections-2026/src/main.collections/list/OperacoesBasicas/Item.java
+++ Collections/IntelliJ/Collections-2026/src/main.collections/list/OperacoesBasicas/Item.java
@@ -1,5 +1,40 @@
 package main.collections.list.OperacoesBasicas;
 
 public class Item {
+    private int code;
+    private String name;
+    private String description;
+    private double price;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
