f037ccf9177d8a3815a9c01edfac242ed2f623f6
caiquegressoni <caiquegressoni@gmail.com>
2026-08-05 15:25:43 -0300

    Changed springfox dependency to OpenAPI

--- SpringBoot/web-api/src/main/java/dio/web_api/doc/SwaggerConfig.java
+++ SpringBoot/web-api/src/main/java/dio/web_api/doc/SwaggerConfig.java
@@ -1,32 +1,19 @@
 package dio.web_api.doc;
 
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.Contact;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
-    private Contact contact(){
-        return new Contact(
-                "Name",
-                "https://localhost:8080",
-                "name@name.com"
-        );
-    }
-    private ApiInfoBuilder informationApi(){
-        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
-
-        apiInfoBuilder.title("Title - Rest API");
-        apiInfoBuilder.description("Description - Rest API");
-        apiInfoBuilder.version("Version - 1.0");
-        apiInfoBuilder.termsOfServiceUrl("Terms-of-service URL");
-        apiInfoBuilder.license("Lincense - TestRestApi");
-        apiInfoBuilder.licenseUrl("https://localhost:8080");
-        apiInfoBuilder.contact(contact());
-
-        return apiInfoBuilder;
+    /*
+    * Na versão 4.0 e 3.0 do springboot não é mais oferecido suporte para o springfox (antigo Swagger)
+    * Hoje nas versões mais recentes do spring utilizamos o OpenApi, onde não é necessario fazer a classe de configuração.
+    * Ficando exclusivamente para casos de personalização.
+     */
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("API Documentation").description("API Documentation"));
     }
 }
